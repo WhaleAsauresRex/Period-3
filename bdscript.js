16c3208df81714071a3e81be607d9254de17abd4
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-08-14 10:33:54 -1000

    Add files via upload

---
+++ bdscript.js
@@ -0,0 +1,7 @@
+function sayHello() {
+  //I am creating a function called sayHello//
+  var userName = prompt("What is your name?")
+  //Create a varible called userName and stores the users information//
+  window.alert(userName + ", Hello");
+  //Popup a window that says hello//
+}
