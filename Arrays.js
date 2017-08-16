acc854b6dcea7b3d0eb46f4bc7822a276235d720
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-08-16 10:57:43 -1000

    Add files via upload

--- Arrays.js
+++ Arrays.js
@@ -1,3 +1,13 @@
 var juniors = ['Reimon','Hana'];
 var seniors = ["Samiyah","Justin","Bryson"];
 window.alert("Seniors"+" "+seniors.length+" "+"Juniors"+" "+juniors.length);
+//This is how you say the name then numbers//
+var food = [" PapaJohns" ," 2papajohns" ," quadpizzas","hello"];
+var students =["Brandon","Matt","Bryson", "fudge", "You"];
+var y = food.length;
+window.alert(students[food.length]);
+//this is how calling a length works//
+var e = prompt("Odd number");
+var a = prompt("Even number");
+window.alert(e * a)
+//This is how you do regular multiplication//
